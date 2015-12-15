19220f07a881982ffcb8f7f547f53fc19a243bc1
Christoph Steefel <cfsteefel@gmail.com>
2015-12-15 14:07:25 -0800

    Added stdDev calculation function

--- calcGradientStats.c
+++ calcGradientStats.c
@@ -2,60 +2,99 @@
  * Author: Christoph Steefel
  * Date: December 12, 2015
  * Purpose: This file contains the gradient calculation code, which creates
- * a gradient array, as well as calculates the standard Deviation and
+ * a resulting edge array, as well as calculates the standard Deviation and
  * mean gradients.
  */
 
 #include "edgeInfo.h"
 #include <string.h>
+#include <math.h>
 
 #define RED 0
 #define GREEN 1
 #define BLUE 2
+#define SQUARE(x) pow(x, 2)
+
+double pow(double x, double y);
 
 /*
  * Function to calculate the gradients across the array, requires the base
  * array, info about the type of the base array, and the width and height
  * of the base array.
  *
- * Returns: Struct containing a pointer to the gradient array, statisitics
- * about the gradient array, and the size of the array.
+ * Returns: Struct containing a pointer to the gradient array, as well as a
+ * mean gradient value.
  */
-gradientData calcGradients(void ** baseArr, long width, long height,
-    char* type)  {
+gradientData * calcGradientMeans(gradientData * data) {
   // initial variables
   double mean = 0;
-  double stdDev = 0;
-  gradientData result;
   // RGBA array type
-  if (!strcmp(type, "RGBA")) {
+  if (!strcmp(data->type, "RGBA")) {
     // array traversal pointer.
-    void * traversal = *baseArr;
+    void * traversal = data->baseArr;
     //counter for the loop
     int counter = 0;
-    while (counter < (width - 1) * (height - 1)){
+    while (counter < (data->width - 1) * (data->height - 1)){
       // convert all values to char arrays, with entries R, G, B, and A
       char curr[4], eastNeighbor[4], southNeighbor[4];
       // Copy the data into char arrays for processing
       memcpy(curr, traversal, 1);
       memcpy(eastNeighbor, traversal + 1, 1);
-      memcpy(southNeighbor, traversal + width, 1);
+      memcpy(southNeighbor, traversal + data->width, 1);
       //add eastern gradient
       mean += (+(curr[RED] - eastNeighbor[RED])) +
               (+(curr[GREEN] - eastNeighbor[GREEN])) +
               (+(curr[BLUE] - eastNeighbor[BLUE])) /
-              ((width - 1) * (height - 1));
+              ((data->width - 1) * (data->height - 1));
       //add southern gradient
       mean += (+(curr[RED] - southNeighbor[RED])) +
               (+(curr[GREEN] - southNeighbor[GREEN])) +
               (+(curr[BLUE] - southNeighbor[BLUE])) /
-              ((width - 1) * (height - 1));
+              ((data->width - 1) * (data->height - 1));
       traversal++;
       counter++;
     }
-    result.mean = mean;
+    data->mean = mean;
   }
-  return result;
+  return data;
 }
 
 
+
+/*
+ * Function to calculate standard deviation, assumes the mean is correct for
+ * the raster data.
+ *
+ * Returns the original array in the gradientData struct, along with a stdDev
+ * value.
+ */
+gradientData * calcStandardDeviation(gradientData * data) {
+  long stdDev = 0;
+  if (!strcmp(data->type, "RGBA")) {
+    // array traversal pointer.
+    void * traversal = data->baseArr;
+    //counter for the loop
+    int counter = 0;
+    while (counter < (data->width - 1) * (data->height - 1)){
+      // convert all values to char arrays, with entries R, G, B, and A
+      char curr[4], eastNeighbor[4], southNeighbor[4];
+      // Copy the data into char arrays for processing
+      memcpy(curr, traversal, 1);
+      memcpy(eastNeighbor, traversal + 1, 1);
+      memcpy(southNeighbor, traversal + data->width, 1);
+      //add eastern gradient
+      stdDev += SQUARE((+(curr[RED] - eastNeighbor[RED])) +
+              (+(curr[GREEN] - eastNeighbor[GREEN])) +
+              (+(curr[BLUE] - eastNeighbor[BLUE]))- data->mean) /
+              ((data->width - 1) * (data->height - 1));
+      //add southern gradient
+      stdDev += SQUARE((+(curr[RED] - southNeighbor[RED])) +
+              (+(curr[GREEN] - southNeighbor[GREEN])) +
+              (+(curr[BLUE] - southNeighbor[BLUE])) - data->mean) /
+              ((data->width - 1) * (data->height - 1));
+      traversal++;
+      counter++;
+    }
+  }
+  return data;
+  }
--- edgeInfo.h
+++ edgeInfo.h
@@ -1,5 +1,9 @@
 typedef struct {
   double mean;
   double stdDev;
+  void ** baseArr;
+  char * type;
+  void ** edgeRaster;
+  long width, height;
 } gradientData;
 
